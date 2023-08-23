872cd8de814e38e00419037984f5326cdb61c025
Pedro Pereira <pedro.pereira@primecontrol.com.br>
2023-08-22 21:57:55 -0300

    Feature: Adicionando controle de stamina

--- src/actors/player/player.gd
+++ src/actors/player/player.gd
@@ -7,16 +7,27 @@ var velocity: Vector2
 export(int) var move_speed
 export(int) var run_speed
 export(int) var gravity_speed
+export(int) var stamina
 
 func _physics_process(delta: float) -> void:
 	move()
+	calc_stamina()
 	jump(delta)
 	velocity = move_and_slide(velocity)
-	sprite.animate(velocity)
+	sprite.animate(velocity, stamina)
+	
+func calc_stamina() -> void:
+	# Se a stamina não estivar no máximo e o player estiver parado ela começa a recuperar
+	if stamina < 150 and velocity.x == 0:
+		stamina += 0.0005
+	# Se a stamina não estivar no máximo, o player estiver andando e não tentando correr ela começa a recuperar mais lento
+	elif stamina < 150 and velocity.x != 0 and Input.get_action_strength("ui_shift") != 1:
+		stamina += 0.00025
 	
 func move() -> void:
-	if Input.get_action_strength("ui_select"):
+	if velocity.x != 0 and Input.get_action_strength("ui_shift") and stamina > 0:
 		velocity.x = run_speed * get_direction()
+		stamina -= 0.05
 	else:
 		velocity.x = move_speed * get_direction()
 			
--- src/scripts/player_texture.gd
+++ src/scripts/player_texture.gd
@@ -1,8 +1,10 @@
 extends Sprite
 
 onready var animation: AnimationPlayer = get_node("%AnimationPlayer")
+var stm
 
-func animate(velocity: Vector2) -> void:
+func animate(velocity: Vector2, stamina: int) -> void:
+	stm = stamina
 	verify_orientation(velocity.x)
 	horizontal_behavior(velocity.x)
 	
@@ -13,7 +15,7 @@ func verify_orientation(speed: float) -> void:
 		flip_h = true
 	
 func horizontal_behavior(speed: float) -> void:
-	if speed != 0 and Input.get_action_strength("ui_select"):
+	if speed != 0 and Input.get_action_strength("ui_shift") and stm > 0:
 		animation.play("run")
 		return
 	elif speed != 0:
