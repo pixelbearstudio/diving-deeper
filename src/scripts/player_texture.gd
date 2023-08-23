1f0e6875cdff235afbbed8029b25476cb6036b7f
Pedro Pereira <pedro.pereira@primecontrol.com.br>
2023-08-22 21:24:19 -0300

    Feature: Adicionando sprite de idle, move e run. Além do script para fazer o controle e troca das sprites.

---
+++ src/scripts/player_texture.gd
@@ -0,0 +1,24 @@
+extends Sprite
+
+onready var animation: AnimationPlayer = get_node("%AnimationPlayer")
+
+func animate(velocity: Vector2) -> void:
+	verify_orientation(velocity.x)
+	horizontal_behavior(velocity.x)
+	
+func verify_orientation(speed: float) -> void:
+	if speed > 0:
+		flip_h = false
+	elif speed < 0:
+		flip_h = true
+	
+func horizontal_behavior(speed: float) -> void:
+	if speed != 0 and Input.get_action_strength("ui_select"):
+		animation.play("run")
+		return
+	elif speed != 0:
+		animation.play("move")
+		return
+	else:
+		animation.play("idle")
+		return
